d5bcc0334035f60fe631ec7d5d5a9f8b363e8c8b
jaywadekar18 <jaywadekar18@gmail.com>
2023-05-02 14:24:38 +0530

    deployment issue

--- server.js
+++ server.js
@@ -46,11 +46,12 @@ app.use("/api/reviews", reviewRouter);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
   const path = require("path");
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "/client/public/index.html"));
   });
 }
 // if (process.env.NODE_ENV == "production") {
